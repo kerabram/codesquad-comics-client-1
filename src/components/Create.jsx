858b25c38ff1c7123c71e9897dfba35fc6669c6c
kerabram <kera.bramwell@gmail.com>
2025-05-05 14:11:10 -0400

    third session

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -1,6 +1,5 @@
 
-
-
+import React from 'react' 
 
 function Create (){
 const handleCreateForm = (e) =>{
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,26 @@
+import React from 'react' 
+
 function Login (){
+    const handleLoginForm = (e) =>{
+      e.preventDefault();
+    console.log("Login Form Submitted");
+    console.log(e.target.username.value);
+    console.log(e.target.password.value);
+    }
+
     return (
-      // Kit: you need to add code here
-      <main>
-          
+    <main>
+       <form className="login-form"  onSubmit={handleLoginForm} >
+                <div className="login-groups">
+                       <label htmlFor="username">Username:</label>
+                      <input type="text" id="username" name="username" value="username" />
+                </div>
+                 <div className="login-groups"   >
+                       <label htmlFor="password"><Password></Password>:</label>
+                      <input type="text" id="password" name="password" value="password" />
+                 </div>
+                <input type="submit" />
+            </form> 
       </main>
     );
 }
